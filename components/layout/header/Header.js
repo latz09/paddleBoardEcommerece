6c94b54481ef074f21ef272f7a0e80406088b2a6
latz09 <85132328+latz09@users.noreply.github.com>
2022-05-11 17:21:54 -0500

    respnsive header

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -4,28 +4,31 @@ import Logo from './Logo';
 
 const Header = () => {
 	return (
-		<div className="flex justify-between border-b-2 items-center font-semibold p-4 uppercase">
-			<Logo />
-			<div className="grid grid-cols-3 space-x-4 text-sm">
+		<>
+			<div className="bg-blue-900 text-white text-sm">
 				<Link href='/'>
-					<a>boards</a>
-				</Link>
-				<Link href='/'>
-					<a>gear</a>
-				</Link>
-				<Link href='/'>
-					<a>explore</a>
-				</Link>
-			</div>
-			<div>
-				<Link href='/'>
-					<div className="flex space-x-2">
+					<div className='flex justify-end px-4 py-1 mr-4'>
 						<span>{shoppingCart}</span>
 						<p>CART</p>
 					</div>
 				</Link>
 			</div>
-		</div>
+			<div className='flex justify-between md:justify-evenly border-b-2 items-center font-semibold p-4 uppercase'>
+				<Logo />
+				<div className='grid grid-cols-3 gap-2 space-x-2 text-sm'>
+					<Link href='/'>
+						<a>boards</a>
+					</Link>
+					<Link href='/'>
+						<a>gear</a>
+					</Link>
+					<Link href='/'>
+						<a>explore</a>
+					</Link>
+				</div>
+			
+			</div>
+		</>
 	);
 };
 
