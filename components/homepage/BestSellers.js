9c05a92db9f04665483a771f373e55cb818b6853
latz09 <85132328+latz09@users.noreply.github.com>
2022-05-16 09:32:04 -0500

    best sellers section completed, using database

--- components/homepage/BestSellers.js
+++ components/homepage/BestSellers.js
@@ -1,13 +1,32 @@
-import { FilteredBestSellers } from "../boards/AllBoards";
+import BoardItem from "../boards/BoardItem";
 
-const BestSellers = () => {
+const BestSellers = ({ data }) => {
+	const allBoards = { data }.data;
+
+	const filteredBoards = allBoards.filter(board => board.isBestSeller === true)
+
+	console.log(filteredBoards)
+	
 	return (
 		<div>
 			<h1 className='font-bold text-2xl tracking-wider text-center'>
 				SHOP BEST SELLERS
 			</h1>
-			<FilteredBestSellers/>
-            
+			<div className='card grid-cols-4'>
+				{filteredBoards.map((x) => (
+					<li key={x.id}>
+						<BoardItem
+							name={x.name}
+							image={x.image.main}
+							length={x.specs.Length}
+							price={x.price}
+							salePrice={x.salePrice}
+						/>
+					</li>
+				))}
+			</div>
+
+		
 		</div>
 	);
 };
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,19 @@ import BestSellers from '../components/homepage/BestSellers';
 import Hero from '../components/homepage/Hero';
 import PioneerSection from '../components/homepage/PioneerSection';
 
-export default function Home() {
+export default function Home({data}) {
+	const boards = data.paddleBoards
+	// console.log(boards)
+
+	
+	
 	return (
 		<>
 			<div>
 				<Hero />
 			</div>
 			<div className='pt-8'>
-				<BestSellers />
+				<BestSellers data={boards}/>
 			</div>
 			<div>
 				<PioneerSection />
@@ -18,18 +23,15 @@ export default function Home() {
 	);
 }
 
-// import { PADDLEBOARDS } from '../data/PaddleBoards';
-// import Image from 'next/image';
-// const hardTopPaddleboards = PADDLEBOARDS.hardTop_PaddleBoards;
-// const inflatablePaddleboards = PADDLEBOARDS.inflatable_paddleBoards;
-// {hardTopPaddleboards.map((paddleBoard1) => (
-//   <li key={paddleBoard1.id} className="text-center">
-//     name: {paddleBoard1.name}
-//     <Image
-//       src={paddleBoard1.image.main}
-//       alt={`Image of ${paddleBoard1.name}`}
-    //   width={500}
-    //   height={500}
-//     />
-//   </li>
-// ))}
+export async function getStaticProps() {
+	const res = await fetch('http://localhost:3000/api/paddleboards');
+
+	const data = await res.json();
+	
+
+	return {
+		props: {
+			data,
+		},
+	};
+}
