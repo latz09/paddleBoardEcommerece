88ab419391910acbcb6cc7af7ef5bb430569c080
latz09 <85132328+latz09@users.noreply.github.com>
2022-06-07 19:41:46 -0500

    moved cart icon over

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 	return (
 		<div className='bg-blue-900 text-white text-sm p-2'>
 			<Link href='/cart'>
-				<div className='flex justify-end items-center space-x-2 cursor-pointer mr-5'>
+				<div className='flex justify-end items-center space-x-2 cursor-pointer sm:mr-5'>
 					<span>{shoppingCart}</span>
 					<p>CART</p>
 					<span className='bg-orange-500 rounded-full py-2 px-4 border-2 border-white font-bold'>
